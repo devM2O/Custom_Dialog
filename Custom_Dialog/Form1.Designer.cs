029ddb451cdf667d91c67efcbf7cde781c6a201f
Min Maw Oo <minmawoo.ucsm@gmail.com>
2024-05-26 03:25:56 +0630

    position

--- Custom_Dialog/Form1.Designer.cs
+++ Custom_Dialog/Form1.Designer.cs
@@ -127,7 +127,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(306, 8);
+            this.button2.Location = new System.Drawing.Point(264, 8);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(130, 55);
             this.button2.TabIndex = 2;
@@ -138,7 +138,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(158, 8);
+            this.button1.Location = new System.Drawing.Point(116, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(130, 55);
             this.button1.TabIndex = 1;
